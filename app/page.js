663bff6bbe489739441f157476499a63b2f50f9f
Moosa Akoodie <moosaakoodie3@gmail.com>
2024-10-24 10:59:31 +0200

    made a dynamic recipe detail page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
+import Link from 'next/link'; 
+import { fetchRecipes } from '../lib/dummyData/fetchSampleData';
 
 export default function Home() {
+  const recipes = fetchRecipes();
 
   return (
     <div>
-      <main><p>123</p></main>
+      <main>
+        <h1>Recipes</h1>
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe._id}>
+              <Link href={`/recipes/${recipe._id}`}>{recipe.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
--- app/recipes/[id]/page.js
+++ app/recipes/[id]/page.js
@@ -1,7 +1,33 @@
-export default function SingleRecipe() {
+export default async function SingleRecipe({ params }) {
+  // Await params to ensure it's resolved before destructuring
+  const { id } = await params; // Await the params here
+  
   return (
     <div>
-      <main>123</main>
+      <main>
+        {/* Display title and description */}
+        <h1>this is our Dynamic Recipe Detail Page</h1>
+
+        {/* Display time and servings */}
+        <div>
+        </div>
+
+        {/* Display recipe tags */}
+        <div>
+        </div>
+
+        {/* Display images */}
+        <div>
+        </div>
+
+        {/* Display ingredients */}
+        <ul>
+        </ul>
+
+        {/* Display instructions */}
+        <div>
+        </div>
+      </main>
     </div>
   );
 }
